3709bcee83ac1c53433250d8c78d617cf2e643c9
sugishitadaichi <fusion.haa0743@outlook.jp>
2023-07-23 19:23:44 +0900

    階層整理後のビルドエラー・タブアイコン画像変更

--- MinimumTimer/Controller/MainAlarmViewController.swift
+++ MinimumTimer/Controller/MainAlarmViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class MainAlarmViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
