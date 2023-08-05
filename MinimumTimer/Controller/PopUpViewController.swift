7e9482df7ba1e508a754cb80e67fb53f8273edb1
sugishitadaichi <fusion.haa0743@outlook.jp>
2023-08-05 17:51:32 +0900

    ポップアップビューの時間入力を時間と分で分け

--- MinimumTimer/Controller/PopUpViewController.swift
+++ MinimumTimer/Controller/PopUpViewController.swift
@@ -9,13 +9,20 @@ import Foundation
 import RealmSwift
 
 class PopUpViewController: UIViewController {
+    //ユーザーが項目に設定した時間（分）を紐付け
+    @IBOutlet weak var userSetupMinutesTimeText: UITextField!
+    //ユーザーが項目に設定した時間（時間）を紐付け
+    @IBOutlet weak var userSetupHourTimeText: UITextField!
+    //キャンセルボタンを押した際の処理を紐付け
     @IBAction func cancelButtonAction(_ sender: Any) {
         //キャンセルすると画面を戻る処理
         dismiss(animated: true, completion: nil)
     }
+    //キャンセルボタンを紐付け
     @IBOutlet weak var cancelButton: UIButton!
+    //追加ボタンを紐付け
     @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var userSetupTimeText: UITextField!
+    //ユーザーが追加した項目名を紐付け
     @IBOutlet weak var userSetupNameText: UITextField!
     
     override func viewDidLoad() {
