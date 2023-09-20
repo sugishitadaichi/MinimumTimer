563704269ba3d208777384d753eef67fc0b1a100
sugishitadaichi <fusion.haa0743@outlook.jp>
2023-09-20 20:59:19 +0900

    アラーム設定画面実装作成・pickerViewデータ渡し模索中2

--- MinimumTimer/Controller/Cell/ItemSelectedFooter.swift
+++ MinimumTimer/Controller/Cell/ItemSelectedFooter.swift
@@ -8,7 +8,9 @@
 import UIKit
 import RealmSwift
 
-class ItemSelectedFooter: UIView, UITextFieldDelegate , UIPickerViewDelegate, UIPickerViewDataSource{
+class ItemSelectedFooter: UIView, UITextFieldDelegate , UIPickerViewDelegate, UIPickerViewDataSource, PopUpViewControllerDelegate {
+    
+    
     //追加ボタンを押した際の処理（Realm導入後。AlarmSettingViewCellに反映させる？）
     @IBAction func addButtonAction(_ sender: UIButton) {
         
@@ -27,8 +29,10 @@ class ItemSelectedFooter: UIView, UITextFieldDelegate , UIPickerViewDelegate, UI
     //項目設定のプロパティ
     var masterItemList: [MasterItem] = []
     //選択データ（項目名＋時間表示）
-    //計算プロパティとしてmasterItemDataを宣言して初期化
-    var masterItemData = ["abcdfg　１時間30分"]
+    //計算型プロパティとしてmasterItemDataを宣言して初期化
+    var masterItemData: String {
+        return "\(masterItem.userSetupName)   \(masterItem.userSetupHourTime)時間 \(masterItem.userSetupMinutesTime)分"
+    }
 
 
     //未処理。//init関数に記載
@@ -37,6 +41,67 @@ class ItemSelectedFooter: UIView, UITextFieldDelegate , UIPickerViewDelegate, UI
         // Initialization code
         
     }
+    
+    //initの実装
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+        //delegateの登録
+        itemSelectedPickerText.delegate = self
+        //UITextField を選択したときに pickerView のキーボードが表示
+        createPickerView()
+        //画面表示時に角丸を実装
+        setupAddButton()
+        //枠線の設定
+        let bottomBorder = CALayer()
+        bottomBorder.frame = CGRect(x: 0, y: self.frame.height - 1, width: self.frame.width, height: 1.0)
+        bottomBorder.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.addSublayer(bottomBorder)
+        //項目が表示されるよう処理を実行
+        setItemSelectedMasterItem()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
+    }
+
+    func loadNib() {
+        guard UINib(nibName: String(describing: type(of: self)), bundle: nil).instantiate(withOwner: self, options: nil).first is UIView else {
+                return
+            }
+        
+        //File's Ownerを登録
+        if let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+            self.addSubview(view)
+        }    }
+    
+    
+    //項目を格納しデータを反映させる
+    func reflectMasterItem() {
+        //項目を格納
+        setItemSelectedMasterItem()
+        //反映(UItextField)
+        itemSelectedPickerText.reloadInputViews()
+    }
+    
+    //項目を格納するためのメソッド
+    func setItemSelectedMasterItem() -> Void {
+        //Realmをインスタンス化
+        let realm = try! Realm()
+        //項目を表示する際の条件(項目設定時間の時間の昇順・分の昇順)
+        let sortProperties = [
+            SortDescriptor(keyPath: "userSetupHourTime", ascending: true),
+            SortDescriptor(keyPath: "userSetupMinutesTime", ascending: true)
+        ]
+        //RealmデータベースからMasterItem情報を取得しidの値で降順にソートする
+        let result = realm.objects(MasterItem.self).sorted(by: sortProperties)
+        //masterItemListに格納
+        masterItemList = Array(result)
+        
+    }
+    
     
     //UITextField を選択したときに pickerView のキーボードが表示されるよう実装
     func createPickerView() {
@@ -74,12 +139,14 @@ class ItemSelectedFooter: UIView, UITextFieldDelegate , UIPickerViewDelegate, UI
     }
     //pickerView に設定するデータを登録する
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return masterItemData[row]
+        return masterItemData
     }
     //pickerView の各種データを選択したときに呼ばれるメソッド
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         //UITextFieldに選択されたデータを表示
-        itemSelectedPickerText.text = masterItemData[row]
+        itemSelectedPickerText.text = masterItemData
+        //項目を格納しデータを反映させる
+        reflectMasterItem()
     }
     //　追加ボタンの仕様
     func setupAddButton() {
@@ -91,38 +158,7 @@ class ItemSelectedFooter: UIView, UITextFieldDelegate , UIPickerViewDelegate, UI
     
 
     
-    //initの実装
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        loadNib()
-        //delegateの登録
-        itemSelectedPickerText.delegate = self
-        //UITextField を選択したときに pickerView のキーボードが表示
-        createPickerView()
-        //画面表示時に角丸を実装
-        setupAddButton()
-        //枠線の設定
-        let bottomBorder = CALayer()
-        bottomBorder.frame = CGRect(x: 0, y: self.frame.height - 1, width: self.frame.width, height: 1.0)
-        bottomBorder.backgroundColor = UIColor.lightGray.cgColor
-        self.layer.addSublayer(bottomBorder)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)!
-        loadNib()
-    }
-
-    func loadNib() {
-        guard UINib(nibName: String(describing: type(of: self)), bundle: nil).instantiate(withOwner: self, options: nil).first is UIView else {
-                return
-            }
-        
-        //File's Ownerを登録
-        if let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView {
-            view.frame = self.bounds
-            self.addSubview(view)
-        }    }
+    
 
 
     
